6ef2334259d06b466f9b8790d34a3903866e8a00
Ecstasy-EC <yixicai@connect.hku.hk>
2020-12-03 16:38:50 +0800

    First Version without test

--- include/kd_tree/kd_tree.cpp
+++ include/kd_tree/kd_tree.cpp
@@ -24,11 +24,17 @@ void KD_TREE::Build(vector<PointType> point_cloud){
         // Release the existing kd-tree pointers
     }
     PCL_Storage = point_cloud;
-    BuildTree(Root_Node, 0, PCL_Storage.size());
+    BuildTree(Root_Node, 0, PCL_Storage.size()-1);
 }
 
-void KD_TREE::Nearest_Search(PointType point, int k_nearest, PointType * Nearest_Points){
-
+void KD_TREE::Nearest_Search(PointType point, int k_nearest, PointType Nearest_Points[]){
+    q = priority_queue<PointType_CMP> (); // Clear the priority queue;
+    Search(Root_Node, k_nearest, point);
+    for (int i=0;i<k_nearest;i++){
+        Nearest_Points[i] = q.top().point;
+        q.pop();
+    }
+    return;
 }
 
 void KD_TREE::Add_Points(vector<PointType> PointToAdd, int PointNum){
@@ -39,8 +45,12 @@ void KD_TREE::Add_Points(vector<PointType> PointToAdd, int PointNum){
 }
 
 void KD_TREE::Delete_Points(vector<PointType> PointToDel, int PointNum){
+    bool flag;
     for (int i=0;i!=PointToDel.size();i++){
-        Delete_by_point(Root_Node, PointToDel[i]);
+        flag = Delete_by_point(Root_Node, PointToDel[i]);
+        if (!flag) {
+            printf("Failed to delete point (%0.3f,%0.3f,%0.3f)", PointToDel[i].x, PointToDel[i].y, PointToDel[i].z);
+        }
     }
     return;
 }
@@ -71,36 +81,143 @@ void KD_TREE::BuildTree(KD_TREE_NODE * root, int l, int r){
         covariance[1] = (PCL_Storage[i].y - average[1]) * (PCL_Storage[i].y - average[1]);  
         covariance[2] = (PCL_Storage[i].z - average[2]) * (PCL_Storage[i].z - average[2]);              
     }
-    // Select the axis with maximal covariance
-    // Divide by the selection axis
-    Pull_Up(root);
-    // Check if rebuild is needed
+    int div_axis = 0;
+    for (i = 1;i<3;i++){
+        if (covariance[i] > covariance[div_axis]) div_axis = i;
+    }
+    root->division_axis = div_axis;
+    switch (div_axis)
+    {
+    case 0:
+        nth_element(PCL_Storage.begin(), PCL_Storage.begin()+mid, PCL_Storage.end(), point_cmp_x);
+        break;
+    case 1:
+        nth_element(PCL_Storage.begin(), PCL_Storage.begin()+mid, PCL_Storage.end(), point_cmp_y);
+        break;
+    case 2:
+        nth_element(PCL_Storage.begin(), PCL_Storage.begin()+mid, PCL_Storage.end(), point_cmp_z);
+        break;
+    default:
+        nth_element(PCL_Storage.begin(), PCL_Storage.begin()+mid, PCL_Storage.end(), point_cmp_x);
+        break;
+    }
+    BuildTree(root->left_son_ptr, l, mid-1);
+    BuildTree(root->right_son_ptr, mid+1, r);
+    Update(root);
+    // In the very first building tree, check is unnecessary as the balance properties is gauranteed.
+    return;
 }
 
 void KD_TREE::Rebuild(KD_TREE_NODE * root){
-
+    // Clear the PCL_Storage vector and release memory
+    vector<PointType> ().swap(PCL_Storage);
+    traverse_for_rebuild(root);
+    BuildTree(root, 0, PCL_Storage.size()-1);
+    return;
 }
 
 void KD_TREE::Delete_by_range(KD_TREE_NODE * root, float x_range[], float y_range[], float z_range[]){
-    // Deal with the condition that: push down label ON and new range delete command arrives -> compare the range and push down?
+    if (root == nullptr) return;
+    if (x_range[0] < root->node_range_x[0] && x_range[1] > root->node_range_x[1] && y_range[0] < root->node_range_y[0] && y_range[1] > root->node_range_y[1] && z_range[0] < root->node_range_z[0] && z_range[1] > root->node_range_z[1]){
+        root->tree_deleted = true;
+        root->point_deleted = true;
+        root->invalid_point_num = root->TreeSize;
+        return;
+    }
+    Delete_by_range(root->left_son_ptr, x_range, y_range, z_range);
+    Delete_by_range(root->right_son_ptr, x_range, y_range, z_range);
+    Update(root);
+    if (Criterion_Check(root)) Rebuild(root);
+    return;
 }
 
-void KD_TREE::Delete_by_point(KD_TREE_NODE * root, PointType point){
-    Push_Down(root);
-    Pull_Up(root);
-    // Check and rebuild    
+bool KD_TREE::Delete_by_point(KD_TREE_NODE * root, PointType point){
+    if (root == nullptr) return;
+    bool flag = false;
+    if (same_point(root->point, point) && !root->point_deleted) {
+        root->point_deleted = true;
+        root->invalid_point_num += 1;
+        if (root->invalid_point_num == root->TreeSize) root->tree_deleted = true;
+        return true;
+    }
+    switch (root->division_axis)
+    {
+    case 0:
+        if (point.x < (root->point.x - EPS)) flag = Delete_by_point(root->left_son_ptr, point);
+            else flag = Delete_by_point(root->right_son_ptr, point);
+        break;
+    case 1:
+        if (point.y < (root->point.y - EPS)) flag = Delete_by_point(root->left_son_ptr, point);
+        else flag = Delete_by_point(root->right_son_ptr, point);
+        break;
+    case 2:
+        if (point.z < (root->point.z - EPS)) flag = Delete_by_point(root->left_son_ptr, point);
+        else flag = Delete_by_point(root->right_son_ptr, point);
+        break;
+    default:
+        if (point.x < (root->point.x - EPS)) flag = Delete_by_point(root->left_son_ptr, point);
+            else flag = Delete_by_point(root->right_son_ptr, point);    
+        break;
+    }
+    Update(root);
+    if (Criterion_Check(root)) Rebuild(root); 
+    return flag;
 }
 
 void KD_TREE::Add(KD_TREE_NODE * root, PointType point){
-    Push_Down(root);
-    Pull_Up(root);
-    // Check and rebuild    
+    if (root == nullptr){
+        root = new KD_TREE_NODE;
+        root->point = point;
+        Update(root);
+        return;
+    }
+    switch (root->division_axis)
+    {
+    case 0:
+        if (point.x < root->point.x) Add(root->left_son_ptr, point);
+            else Add(root->right_son_ptr, point);
+        break;
+    case 1:
+        if (point.y < root->point.y ) Add(root->left_son_ptr, point);
+        else Add(root->right_son_ptr, point);
+        break;
+    case 2:
+        if (point.z < root->point.z) Add(root->left_son_ptr, point);
+        else Add(root->right_son_ptr, point);
+        break;
+    default:
+        if (point.x < root->point.x) Add(root->left_son_ptr, point);
+            else Add(root->right_son_ptr, point);    
+        break;
+    }    
+    Update(root);
+    if (Criterion_Check(root)) Rebuild(root); 
+    return;
 }
 
-void KD_TREE::Search(KD_TREE_NODE * root, int k_nearest){
-    Push_Down(root);
-    Pull_Up(root);
-    // Check and rebuild
+void KD_TREE::Search(KD_TREE_NODE * root, int k_nearest, PointType point){
+    if (root == nullptr) return;
+    float dist = calc_dist(point, root->point);
+    if (q.size() < k_nearest || dist < q.top().dist){
+        q.top();
+        PointType_CMP current_point{point, dist};
+        q.push(current_point);
+    }
+    float dist_left_node = calc_box_dist(root->left_son_ptr, point);
+    float dist_right_node = calc_box_dist(root->right_son_ptr, point);
+    if (q.size()< k_nearest || dist_left_node < q.top().dist && dist_right_node < q.top().dist){
+        if (dist_left_node < dist_right_node) {
+            Search(root->left_son_ptr, k_nearest, point);
+            if (q.size() < k_nearest || dist_right_node < q.top().dist) Search(root->right_son_ptr, k_nearest, point);
+        } else {
+            Search(root->right_son_ptr, k_nearest, point);
+            if (q.size() < k_nearest || dist_left_node < q.top().dist) Search(root->left_son_ptr, k_nearest, point);
+        }
+    } else {
+        if (dist_left_node < q.top().dist) Search(root->left_son_ptr, k_nearest, point);
+        if (dist_right_node < q.top().dist) Search(root->right_son_ptr, k_nearest, point);
+    }
+    return;
 }
 
 bool KD_TREE::Criterion_Check(KD_TREE_NODE * root){
@@ -122,92 +239,7 @@ bool KD_TREE::Criterion_Check(KD_TREE_NODE * root){
     return false;
 }
 
-void KD_TREE::Push_Down(KD_TREE_NODE * root){
-    if (!root->delete_range_need_push_down){
-        return;
-    }
-    KD_TREE_NODE * left_son_ptr = root->left_son_ptr;
-    KD_TREE_NODE * right_son_ptr = root->right_son_ptr;
-    if (left_son_ptr != nullptr){
-        left_son_ptr->delete_range_need_push_down = true;
-        switch (root->division_axis)
-        {
-        case 0:
-            left_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            left_son_ptr->delete_range_x[1] = root->point.x;
-            left_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            left_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            left_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            left_son_ptr->delete_range_z[1] = root->delete_range_z[1];                       
-            break;
-        case 1:
-            left_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            left_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            left_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            left_son_ptr->delete_range_y[1] = root->point.y;   
-            left_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            left_son_ptr->delete_range_z[1] = root->delete_range_z[1];          
-            break;
-        case 2:
-            left_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            left_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            left_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            left_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            left_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            left_son_ptr->delete_range_z[1] = root->point.z;          
-            break;
-        default:
-            left_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            left_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            left_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            left_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            left_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            left_son_ptr->delete_range_z[1] = root->delete_range_z[1];           
-            break;
-        }
-    }
-    if (right_son_ptr != nullptr){
-        right_son_ptr->delete_range_need_push_down = true;
-        switch (root->division_axis)
-        {
-        case 0:
-            right_son_ptr->delete_range_x[0] = root->point.x;
-            right_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            right_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            right_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            right_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            right_son_ptr->delete_range_z[1] = root->delete_range_z[1];                       
-            break;
-        case 1:
-            right_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            right_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            right_son_ptr->delete_range_y[0] = root->point.y;  
-            right_son_ptr->delete_range_y[1] = root->delete_range_y[1]; 
-            right_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            right_son_ptr->delete_range_z[1] = root->delete_range_z[1];          
-            break;
-        case 2:
-            right_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            right_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            right_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            right_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            right_son_ptr->delete_range_z[0] = root->point.z;  
-            right_son_ptr->delete_range_z[1] = root->delete_range_z[1];        
-            break;
-        default:
-            right_son_ptr->delete_range_x[0] = root->delete_range_x[0];
-            right_son_ptr->delete_range_x[1] = root->delete_range_x[1];
-            right_son_ptr->delete_range_y[0] = root->delete_range_y[0];
-            right_son_ptr->delete_range_y[1] = root->delete_range_y[1];   
-            right_son_ptr->delete_range_z[0] = root->delete_range_z[0];
-            right_son_ptr->delete_range_z[1] = root->delete_range_z[1];           
-            break;
-        }        
-    }
-    root->delete_range_need_push_down = false;
-}
-
-void KD_TREE::Pull_Up(KD_TREE_NODE* root){
+void KD_TREE::Update(KD_TREE_NODE* root){
     KD_TREE_NODE * left_son_ptr = root->left_son_ptr;
     KD_TREE_NODE * right_son_ptr = root->right_son_ptr;
     // Update Tree Size
@@ -254,3 +286,35 @@ void KD_TREE::Pull_Up(KD_TREE_NODE* root){
     }
     return;
 }
+
+void KD_TREE::traverse_for_rebuild(KD_TREE_NODE * root){
+    if (root == nullptr || root->tree_deleted) return;
+    if (!root->point_deleted) PCL_Storage.push_back(root->point);
+    traverse_for_rebuild(root->left_son_ptr);
+    traverse_for_rebuild(root->right_son_ptr);
+    return;
+}
+
+bool KD_TREE::same_point(PointType a, PointType b){
+    return (fabs(a.x-b.x) < EPS &&fabs(a.y-b.y) < EPS && fabs(a.z-b.z) < EPS );
+}
+
+float KD_TREE::calc_dist(PointType a, PointType b){
+    float dist = 0.0f;
+    dist = (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y) + (a.z-b.z)*(a.z-b.z);
+    return dist;
+}
+
+float KD_TREE::calc_box_dist(KD_TREE_NODE * node, PointType point){
+    float min_dist, x_min_dist, y_min_dist, z_min_dist;
+    x_min_dist = min((point.x - node->node_range_x[0])*(point.x - node->node_range_x[0]), (point.x - node->node_range_x[1])*(point.x - node->node_range_x[1]));
+    y_min_dist = min((point.y - node->node_range_y[0])*(point.y - node->node_range_y[0]), (point.y - node->node_range_y[1])*(point.y - node->node_range_y[1]));
+    z_min_dist = min((point.z - node->node_range_z[0])*(point.z - node->node_range_z[0]), (point.z - node->node_range_z[1])*(point.z - node->node_range_z[1]));
+    min_dist = min(x_min_dist, min(y_min_dist, z_min_dist));
+    return min_dist;
+}
+
+bool KD_TREE::point_cmp_x(PointType a, PointType b) { return a.x < b.x;}
+bool KD_TREE::point_cmp_y(PointType a, PointType b) { return a.y < b.y;}
+bool KD_TREE::point_cmp_z(PointType a, PointType b) { return a.z < b.z;}
+
--- include/kd_tree/kd_tree.h
+++ include/kd_tree/kd_tree.h
@@ -1,4 +1,8 @@
 #include <pcl/point_types.h>
+#include <stdio.h>
+#include <queue>
+
+#define EPS 1e-4
 
 using namespace std;
 
@@ -11,12 +15,22 @@ struct KD_TREE_NODE
     int TreeSize = 1;
     int invalid_point_num = 0;
     bool point_deleted = false;
-    bool tree_deleted = false;
-    bool delete_range_need_push_down = false;    
-    float node_range_x[2], node_range_y[2], node_range_z[2];
-    float delete_range_x[2], delete_range_y[2], delete_range_z[2];     // First index: 0~2 division axis x,y,z; Second index: 0~1 min max of the range
+    bool tree_deleted = false; 
+    float node_range_x[2], node_range_y[2], node_range_z[2];   
     KD_TREE_NODE *left_son_ptr = nullptr;
     KD_TREE_NODE *right_son_ptr = nullptr;
+};
+
+struct PointType_CMP{
+    PointType point;
+    float dist;
+    PointType_CMP (PointType p, float d){
+        this->point = p;
+        this->dist = d;
+    };
+    bool operator()(const PointType_CMP & a, const PointType_CMP & b){
+        return a.dist < b.dist;
+    }
 };
 
 class KD_TREE
@@ -26,16 +40,23 @@ private:
     float balance_criterion_param;
     KD_TREE_NODE * Root_Node;
     vector<PointType> PCL_Storage;
+    priority_queue<PointType_CMP> q;
     void BuildTree(KD_TREE_NODE * root, int l, int r);
     void Rebuild(KD_TREE_NODE * root);
     void Delete_by_range(KD_TREE_NODE * root, float x_range[], float y_range[], float z_range[]);
-    void Delete_by_point(KD_TREE_NODE * root, PointType point);
+    bool Delete_by_point(KD_TREE_NODE * root, PointType point);
     void Add(KD_TREE_NODE * root, PointType point);
-    void Search(KD_TREE_NODE * root, int k_nearest);
+    void Search(KD_TREE_NODE * root, int k_nearest, PointType point);
     bool Criterion_Check(KD_TREE_NODE * root);
     void Push_Down(KD_TREE_NODE * root);
-    void Pull_Up(KD_TREE_NODE * root);
-    void update_range(KD_TREE_NODE * root);
+    void Update(KD_TREE_NODE * root);
+    void traverse_for_rebuild(KD_TREE_NODE * root); 
+    bool same_point(PointType a, PointType b);
+    float calc_dist(PointType a, PointType b);
+    float calc_box_dist(KD_TREE_NODE * node, PointType point);
+    bool point_cmp_x(PointType a, PointType b); 
+    bool point_cmp_y(PointType a, PointType b); 
+    bool point_cmp_z(PointType a, PointType b); 
 
 public:
     KD_TREE(float delete_criterion_param = 0.50f, float balance_criterion_param = 0.70f);
