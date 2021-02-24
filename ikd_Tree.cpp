2b64046835b2fc92469ea663c847f4d3659bf0ed
Ecstasy-EC <yixicai@connect.hku.hk>
2021-02-25 00:01:21 +0800

    Retest search and downsample

--- ikd_Tree.cpp
+++ ikd_Tree.cpp
@@ -267,7 +267,7 @@ void KD_TREE::multi_thread_rebuild(){
         pthread_mutex_unlock(&rebuild_ptr_mutex_lock);         
         pthread_mutex_lock(&termination_flag_mutex_lock);
         terminated = termination_flag;
-        pthread_mutex_unlock(&termination_flag_mutex_lock);          
+        pthread_mutex_unlock(&termination_flag_mutex_lock);
         usleep(100); 
     }
     printf("Rebuild thread terminated normally\n");    
