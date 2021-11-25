2b2732cdc8ef7ed59d28f8cd3b44702561b7ab12
Hyungtae Lim <shapelim@kaist.ac.kr>
2021-11-26 01:48:17 +0900

    Seperate examples and include files

---
+++ cmake-build-release/CMakeFiles/ikd_tree_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ikd_tree_demo.dir/examples/ikd_Tree_demo.cpp.o"
+  "CMakeFiles/ikd_tree_demo.dir/ikd_Tree/ikd_Tree.cpp.o"
+  "ikd_tree_demo"
+  "ikd_tree_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ikd_tree_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-release/CMakeFiles/ikd_tree_demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ikd_tree_demo.
+# This may be replaced when dependencies are built.
